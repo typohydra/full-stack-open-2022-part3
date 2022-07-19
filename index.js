adec96f2ff76c29ff5ebfd30db5451cc676f99f5
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-19 18:25:54 +0400

    phonebook backend 3.6

--- index.js
+++ index.js
@@ -58,9 +58,21 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 app.post('/api/persons', (req, res) => {
-  const id = Math.floor(Math.random() * 1000000000)
   const body = req.body
+  
+  if(!body.name || !body.number) {
+    return res.status(400).json({
+      error: `name and/or number missing`
+    })
+  }
+  if(persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: `name must be unique`
+    })
+  }
 
+  const id = Math.floor(Math.random() * 1000000000)
+  
   const person = {
     id: id,
     name: body.name,
