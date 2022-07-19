2941929a44388521b9a92880db09564134a6e418
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-19 19:16:49 +0400

    phonebook backend 3.8

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
 let persons = 
 [
