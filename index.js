f8e1240ab5d73612b8f7521ca9b80747ee133840
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-20 17:05:48 +0400

    phonebook full stack 3.11 frontend production build

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) :
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = 
 [
